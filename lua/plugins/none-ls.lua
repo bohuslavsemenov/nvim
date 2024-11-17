da24c391d10183fae9f928e2ccd0ad633397f534
Bohuslav Semenov <bohuslav.semenov@gmail.com>
2024-11-17 13:27:52 +0100

    update config

--- lua/plugins/none-ls.lua
+++ lua/plugins/none-ls.lua
@@ -1,16 +1,58 @@
+local filetypes = {
+	"javascript",
+	"javascriptreact",
+	"typescript",
+	"typescriptreact",
+	"vue",
+	"css",
+	"scss",
+	"less",
+	"html",
+	"json",
+	"jsonc",
+	"yaml",
+	"markdown",
+	"markdown.mdx",
+	"graphql",
+	"handlebars",
+	"svelte",
+}
+
 return {
 	"nvimtools/none-ls.nvim",
+	dependencies = {
+		"nvimtools/none-ls-extras.nvim",
+	},
 	config = function()
 		local null_ls = require("null-ls")
+		local augroup = vim.api.nvim_create_augroup("LspFormatting", {})
 		null_ls.setup({
 			sources = {
-				null_ls.builtins.code_actions.eslint_d,
-
-				null_ls.builtins.diagnostics.eslint_d,
+				require("none-ls.code_actions.eslint_d").with({
+					filetypes,
+				}),
+				require("none-ls.diagnostics.eslint_d").with({
+					filetypes,
+				}),
 
 				null_ls.builtins.formatting.stylua,
-				null_ls.builtins.formatting.prettier,
+				null_ls.builtins.formatting.prettier.with({
+					filetypes,
+				}),
+				null_ls.builtins.formatting.golines,
 			},
+			on_attach = function(client, bufnr)
+				if client.supports_method("textDocument/formatting") then
+					vim.api.nvim_clear_autocmds({ group = augroup, buffer = bufnr })
+					vim.api.nvim_create_autocmd("BufWritePre", {
+						group = augroup,
+						buffer = bufnr,
+						callback = function()
+							vim.lsp.buf.format({ bufnr = bufnr, async = false })
+						end,
+					})
+				end
+			end,
 		})
 
 		vim.keymap.set("n", "<leader>gf", vim.lsp.buf.format, {})
--- lua/vim-options.lua
+++ lua/vim-options.lua
@@ -4,5 +4,6 @@ vim.cmd("set softtabstop=2")
 vim.cmd("set shiftwidth=2")
 vim.cmd("set number relativenumber")
 vim.cmd("set autoindent")
+vim.cmd("language en_US")
 
 vim.g.mapleader = " "
